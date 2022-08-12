831dc2a1654a2ddc4cfb6bfe2ac0806af85db758
Francis Beeson <leobeeson@gmail.com>
2022-08-12 22:46:26 +0100

    Generate list of neighbourhoods per city in California

--- extract_california_taxonomy.py
+++ extract_california_taxonomy.py
@@ -1,8 +1,16 @@
+from collections import defaultdict
 import csv
+import re
+import json
 
 cities_filepath = "data/cities_in_california.csv"
 neighbourhoods = "data/neighbourhoods_by_city.csv"
 
+prefix = "Neighborhoods in "
+suffix = ", California"
+
+
+# Get Cities
 cities = []
 with open(cities_filepath, "r") as file_input:
     csv_reader = csv.reader(file_input)
@@ -14,11 +22,45 @@ with open(cities_filepath, "r") as file_input:
         cities.append(row[1])
         line_count += 1
 
-
-prefix = "Neighborhoods in "
-suffix = ", California"
-
 clean_cities = []
 for city in cities:
     city = city.removeprefix(prefix)
-    city = city[:len(suffix)]
+    city = city.removesuffix(suffix)
+    clean_cities.append(city)
+
+with open("data/cities_in_california.txt", "w") as output_file:
+    output_file.write("\n".join(clean_cities))
+
+
+# Get Neighbourhoods
+pattern = re.compile("\s+\(.+\)*.*")
+neighbourhoods_per_city = defaultdict(set)
+with open(neighbourhoods, "r") as file_input:
+    csv_reader = csv.reader(file_input)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0 or line_count == 1:
+            line_count += 1
+            continue
+        if "neighborhood" in row[1].lower():
+            continue
+        city = row[0].removeprefix(prefix)
+        city = city.removeprefix(prefix)
+        city = city.removesuffix(suffix)
+        neighbourhood = row[1]
+        neighbourhood = neighbourhood.split(",")[0]
+        neighbourhood = pattern.sub("", neighbourhood)
+        if "/" in neighbourhood:
+            neighbourhoods_split = [n.strip() for n in neighbourhood.split("/")]
+            neighbourhoods_per_city[city].update(neighbourhoods_split)
+        else:
+            neighbourhoods_per_city[city].add(neighbourhood)
+        line_count += 1
+
+neighbourhoods_per_city_json = json.dump(
+    neighbourhoods_per_city, 
+    default=lambda x: list(x) if isinstance(x, set) else x
+    )
+
+with open("data/neighbourhoods_by_city_in_california.json", "w") as output_file:
+    json.dump(neighbourhoods_per_city, output_file, default=lambda x: list(x) if isinstance(x, set) else x)
